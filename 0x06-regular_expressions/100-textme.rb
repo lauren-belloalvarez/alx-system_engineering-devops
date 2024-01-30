4813abcdbd13d1efb8a3dbb6c2d939a2029c0bcd
lauren-belloalvarez <lauren2104ba@yahoo.com>
2024-01-30 04:33:25 -0800

    Add corrected Task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-sender = ARGV[0].scan(/from:(.*?)/)
-receiver = ARGV[0].scan(/to:(.*?)/)
-flags = ARGV[0].scan(/flags:(.*?)/)
+sender = ARGV[0].scan(/from:(.*?)\]/)
+receiver = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
 
 puts [sender, receiver, flags].join(',')
